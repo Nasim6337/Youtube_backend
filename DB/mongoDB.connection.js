cfcaf7c500bdac628adbfbdb733b8e02838a5840
Nasim6337 <163528075+Nasim6337@users.noreply.github.com>
2025-03-20 09:47:58 +0530

    Update mongoDB.connection.js

--- DB/mongoDB.connection.js
+++ DB/mongoDB.connection.js
@@ -1,12 +1,17 @@
 const mongoose=require('mongoose');
 
 const DB=async()=>{
-    try{
-        await mongoose.connect(`mongodb+srv://mnas57189:${process.env.MONGO_PASS}@you-tube.yjsf7.mongodb.net/?retryWrites=true&w=majority&appName=you-tube`).then(()=>{
-            console.log("connection established")
-        })
-    }catch(err){
-      
+ try {
+        await mongoose.connect(`mongodb+srv://mnas57189:${process.env.MONGO_PASS}@you-tube.yjsf7.mongodb.net/?retryWrites=true&w=majority&appName=you-tube`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 20000, 
+            socketTimeoutMS: 45000,
+        });
+        console.log("MongoDB connection established");
+    } catch (err) {
+        console.error("MongoDB Connection Error:", err.message);
+        process.exit(1); // Exit the process if MongoDB fails to connect
     }
 }
 
